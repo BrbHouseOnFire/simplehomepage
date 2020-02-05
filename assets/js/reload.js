5f03d47094797e9e0e89f14578a0950109134fa1
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-02-04 19:17:09 -0600

    new particle interactive background and aesthetic redo

---
+++ assets/js/reload.js
@@ -0,0 +1,12 @@
+
+$(".intro").addClass("go");
+  
+$(".reload").click(function() {
+  $(".intro")
+    .removeClass("go")
+    .delay(200)
+    .queue(function(next) {
+      $(".intro").addClass("go");
+      next();
+    });
+});
