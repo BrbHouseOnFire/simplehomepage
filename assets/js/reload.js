ac4138b796bfd5d632682d5d132bf7277f2f2e1d
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-03-22 17:32:30 -0500

    particle ON OFF button, text adjustments, button arrangement

--- assets/js/reload.js
+++ assets/js/reload.js
@@ -1,12 +1,137 @@
 
 $(".intro").addClass("go");
-  
-$(".reload").click(function() {
+$(".reload").click(() => {
   $(".intro")
     .removeClass("go")
     .delay(200)
-    .queue(function(next) {
+    .queue(next => {
       $(".intro").addClass("go");
       next();
     });
+});
+let flip = 2;
+$(".disableParticles").click(() => {
+  flip++;
+  if (flip % 2 === 1) {
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 0
+        }
+      }
+    });
+  }
+  else {
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 200,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ff0000"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#ff0000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 1,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 0,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 110,
+          // "color": "#FFFFFF",
+          "color": "#2E2E2E",
+          "opacity": 0.4,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 5,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "bounce",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": false,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 200,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 200,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 150,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    });
+  }
 });
